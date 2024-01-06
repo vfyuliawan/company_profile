7e2360b0cef9e515b723c59ae9211718e0f3823c
vicky fadilla <vicky.yuliawan@ist.id>
2024-01-06 23:02:41 +0700

    split-featurs

---
+++ app/component/brand/BrandView.tsx
@@ -0,0 +1,42 @@
+export const BrandView = () => {
+    return(
+        <section id="brand" className="brand">
+        <div className="container">
+          <div className="brand-area">
+            <div className="owl-carousel owl-theme brand-item">
+              <div className="item">
+                <a href="#">
+                  <img src="assets/images/brand/br1.png" alt="brand-image" />
+                </a>
+              </div>{/*/.item*/}
+              <div className="item">
+                <a href="#">
+                  <img src="assets/images/brand/br2.png" alt="brand-image" />
+                </a>
+              </div>{/*/.item*/}
+              <div className="item">
+                <a href="#">
+                  <img src="assets/images/brand/br3.png" alt="brand-image" />
+                </a>
+              </div>{/*/.item*/}
+              <div className="item">
+                <a href="#">
+                  <img src="assets/images/brand/br4.png" alt="brand-image" />
+                </a>
+              </div>{/*/.item*/}
+              <div className="item">
+                <a href="#">
+                  <img src="assets/images/brand/br5.png" alt="brand-image" />
+                </a>
+              </div>{/*/.item*/}
+              <div className="item">
+                <a href="#">
+                  <img src="assets/images/brand/br6.png" alt="brand-image" />
+                </a>
+              </div>{/*/.item*/}
+            </div>{/*/.owl-carousel*/}
+          </div>{/*/.clients-area*/}
+        </div>{/*/.container*/}
+      </section>
+    )
+}
---
+++ app/component/service/ServiceView.tsx
@@ -0,0 +1,47 @@
+
+
+export const ServiceVieiw = () => {
+    return (
+        <section id="service" className="service">
+        <div className="container">
+          <div className="service-content">
+            <div className="row">
+              <div className="col-md-4 col-sm-6">
+                <div className="single-service-item">
+                  <div className="single-service-icon">
+                    <i className="flaticon-car" />
+                  </div>
+                  <h2><a href="#">largest dealership <span> of</span> car</a></h2>
+                  <p>
+                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut den fugit sed quia.  
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4 col-sm-6">
+                <div className="single-service-item">
+                  <div className="single-service-icon">
+                    <i className="flaticon-car-repair" />
+                  </div>
+                  <h2><a href="#">unlimited repair warrenty</a></h2>
+                  <p>
+                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut den fugit sed quia.  
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4 col-sm-6">
+                <div className="single-service-item">
+                  <div className="single-service-icon">
+                    <i className="flaticon-car-1" />
+                  </div>
+                  <h2><a href="#">insurence support</a></h2>
+                  <p>
+                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut den fugit sed quia. 
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>{/*/.container*/}
+      </section>
+    )
+}
