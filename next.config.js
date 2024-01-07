7ed0385c86359ea7af2d939db88a0492a58e6936
vicky fadilla <vicky.yuliawan@ist.id>
2024-01-07 22:43:45 +0700

    oke

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/<company_profile>' : '',
+
+}
 
 module.exports = nextConfig
